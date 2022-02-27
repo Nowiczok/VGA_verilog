ef30ecabc35c795ac5191b942ec65391d2307de4
Nowiczok <56558569+Nowiczok@users.noreply.github.com>
2022-02-27 18:25:59 +0100

    Delete VGA_mk2 directory

--- VGA_mk2/VGA_mk2/compile/vcom.cmd
+++
@@ -1,1 +0,0 @@
--w -erf C:/My_Designs/VGA_mk2/VGA_mk2/compile/VGA_mk2.erf -d C:/My_Designs/VGA_mk2/VGA_mk2/compile -s C:/My_Designs/VGA_mk2/library.cfg -j C:/My_Designs/VGA_mk2/VGA_mk2/projlib.cfg -work VGA_mk2 -O3 -e 100 -work VGA_mk2 -2002 ./../compile/top.vhd 
--- VGA_mk2/VGA_mk2/default_compile.do
+++
@@ -1,4 +0,0 @@
-alog -O2 -protect 0 -msg 5 -sve "$dsn/src/sync_generator.v"
-alog -O2 -protect 0 -msg 5 -sve "$dsn/src/vga_mod.v"
-alog -O2 -protect 0 -msg 5 -sve "$dsn/src/vga_mod_tb.v"
-alog -O2 -protect 0 -msg 5 -sve "$dsn/src/testbenches/sync_generator_tb.v"
